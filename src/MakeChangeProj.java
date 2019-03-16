59b44fb2f2d2f6d67b50e4f9159d77278cda7dc7
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-03-16 12:58:42 -0600

    Working on change method

--- src/MakeChangeProj.java
+++ src/MakeChangeProj.java
@@ -23,21 +23,39 @@ public class MakeChangeProj {
 
 	public static void change(double tend, double price1) {
 		double change = (double) (tend - price1);
+		int change1 = (int) (change * 100);
+		int dollar1 = 0, dollar, five2, ten1, ten2, cent10,cent102, cent1,cent12;
 
-		if (change > 10.00) {
-			int ten2 = (int) (change / 10);
-			double ten1 = change % 10.00;
+//		System.out.println(change1);
+
+		if (change1 > 1000) {
+			 ten2 = (int) (change1 / 1000);
+			 ten1 = change1 % 1000;
+//			System.out.println(ten1);
 			System.out.println(ten2 + " ten dollar bills");
 
-			if (ten1 % 5.0 == 0) {
-				int five1 = (int)(ten1 / 5.0);
+			if (ten1 % 500 == 0) { 
+				double five1 = ten1 / 5.00;
 				System.out.println(five1 + " five dollar bills");
-				if (ten1 % 5.0 != 0) {
-					double five2 = ten1 % 5.0;
-					System.out.println(five2);
-				}
 			}
+			if (ten1 % 500 != 0) {
+				five2 = ten1 % 500;
+				System.out.println(five2 + " 5 dollar bills");
 
+//				System.out.println(five2);
+			}
+			if (ten1 < 500) {
+				dollar = ten1 / 100;
+				dollar1 = ten1 % 100;
+				System.out.println(dollar + " 1 dollar bills");
+			}
+			if (dollar1 < 100) {
+				cent10 = dollar1 / 100;
+				cent102 = dollar1 % 100;
+				System.out.println(cent10 + "dimes");
+
+			}
 		}
 	}
+
 }
---
+++ src/trans.java
@@ -0,0 +1,12 @@
+
+public class trans {
+	public static void main(String[] args) {
+//		double num1;
+		double num2 = 0.50, num3 = 0.30, num4;
+		
+		int num1 = (int)(num2 - num3);
+//		num4 = num1 * 100;
+		System.out.println(num1);
+	}
+
+}
